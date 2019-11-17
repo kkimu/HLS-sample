9068af64154137c497911c79627e9e1fab882a7e
kkimu <kimura.kzm@gmail.com>
2019-11-17 23:22:27 +0900

    Refactor

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ MEDIA_FILE_PATH=server/media
 
 rm -rf ${MEDIA_FILE_PATH}
 mkdir -p ${MEDIA_FILE_PATH}
-mediafilesegmenter -t 5 -b http://localhost:8080 -f ${MEDIA_FILE_PATH} -i index.m3u8 files/sample.mp4
+mediafilesegmenter -t 5 -b http://localhost:8080 -f ${MEDIA_FILE_PATH} -i index.m3u8 -B video- files/sample.mp4
