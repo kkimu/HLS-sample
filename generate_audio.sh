b5451dd3db221a12240555773c31da4be15cfd16
kkimu <kimura.kzm@gmail.com>
2019-11-25 23:56:41 +0900

    Update generate script

--- generate_audio.sh
+++ generate_audio.sh
@@ -2,6 +2,15 @@
 
 MEDIA_FILE_PATH=server/media
 
-rm -rf ${MEDIA_FILE_PATH}
-mkdir -p ${MEDIA_FILE_PATH}
-ffmpeg -i files/sample.mp3 -c:a aac -b:a 64k -vn -hls_list_size 0 -hls_base_url http://localhost:8080/ server/media/index.m3u8
+#rm -rf ${MEDIA_FILE_PATH}
+#mkdir -p ${MEDIA_FILE_PATH}
+
+# Ref http://mobilehackerz.jp/archive/wiki/index.php?%BA%C7%BF%B7ffmpeg%A4%CE%A5%AA%A5%D7%A5%B7%A5%E7%A5%F3%A4%DE%A4%C8%A4%E1
+ffmpeg \
+    -i files/sample.mp3 \
+    -codec:a aac \
+    -b:a 64k \
+    -vn \
+    -hls_list_size 0 \
+    -hls_base_url http://localhost:8080/ \
+    server/media/index.m3u8
